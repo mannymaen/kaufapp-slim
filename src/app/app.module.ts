ece123e96555c79fc1c4f73dd6b88e115e5b589b
Emanuel Ledesma <34849198+mannymaen@users.noreply.github.com>
2022-05-05 10:31:23 -0500

    Changes
    Add nav component skeleton
    Add sidebar component skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentComponent } from './layout/content/content.component';
+import { NavComponent } from './layout/nav/nav.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    NavComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
